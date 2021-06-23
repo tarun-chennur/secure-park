83034cb0167e915d4ce45f3d2f2e4f02af03083a
tarun-chennur <chennur.tarun@gmail.com>
2021-06-23 21:09:40 +0530

    Prelim for the blacklist queue, fixed checking for valid users

--- secureparkmain.py
+++ secureparkmain.py
@@ -1,14 +1,15 @@
 from pyasn1.codec.ber.decoder import PrintableStringDecoder
 import pyrebase
 import time
-import keys.py
+from keys import config
 
 
 
 firebase = pyrebase.initialize_app(config)
 db=firebase.database()
 
-userarr=["ADAD"]# make queue for outbound cars
+userarr=["ADAD"]# cars that have entered 
+# make queue for outbound cars
 
 
 
@@ -23,34 +24,47 @@ userarr=["ADAD"]# make queue for outbound cars
 uid = "UID0001" #Inbound uid
 #timestamp = time.time() 
 #print(timestamp)
-def check():
-    
-    if uid == db.child("user-db").child(uid).get().key(): # Car has just entered, confirm if valid user
+def check(): #this should recieve the uid, list and queue exceed it in terms of scope
+    ids=db.child("user-db").shallow().get().val()
+    print(ids)
+    list_users=[]
+    for i in ids:
+        list_users.append(i)
+    if (uid in list_users): # Car has just entered, confirm if valid user
         print("Valid User")
         if uid in userarr: # check in local hashset/array
         
-            
+            calcndel()
 
             #get starting time from daily report and timenow-starting time ,calc cost
             #delete this user
             # ignore this uid for like 5 mins
-            print("bleh")
-            userarr.remove(uid)
-            db.child("daily-report").child(uid).remove()
-            
+           
             #keep in hashset for 5mins
         else:  
-            db.child("daily-report").child(uid).set(time.time())
-            userarr.add(uid) 
-            print(userarr)
+            registration()
             
             # also put into the hashset
             # ignore this uid for like 5 mins
     
 
 def calcndel():
-def registration():  
+   print("bleh")
+   userarr.remove(uid)
+   db.child("daily-report").child(uid).remove()
+   # append to the outbound car queue
+            
 
+
+def registration():  
+    if in blacklistqueue break
+    db.child("daily-report").child(uid).set(time.time())
+    userarr.append(uid) 
+    print(userarr)
+
+
+
+check()
 
 
 
